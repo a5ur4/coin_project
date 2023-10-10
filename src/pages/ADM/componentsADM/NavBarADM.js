1e0a152517126e26fc916fac2635c01476cef93d
Jhon Victor <116857366+Jhon-Victor-Ramos@users.noreply.github.com>
2023-10-10 12:31:39 -0300

    Alterando NavBar

--- src/pages/ADM/componentsADM/NavBarADM.js
+++ src/pages/ADM/componentsADM/NavBarADM.js
@@ -1,46 +1,54 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom'; // Importe o componente Link
-import appLogo from '../../../images/appLogo.png';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom"; // Importe o componente Link
+import appLogo from "../../../images/appLogo.png";
 
 function NavBarADM() {
-    return (
-        <>
-        <Navbar bg="dark" data-bs-theme="dark">
-            <Container>
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <div class="d-flex">
             <Navbar.Brand as={Link} to="/">
-            <img
+              <img
                 alt=""
                 src={appLogo}
                 width="30"
                 height="30"
-                className="d-inline-block align-top"
-            />{' '}
-            VTC Bank
+                className="d-inline-block align-center"
+              />{" "}
+              VTC Bank
             </Navbar.Brand>
             <Nav className="me-auto">
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to="/ListaMembros">Lista de membros</Nav.Link>
-                <Nav.Link as={Link} to="/ComissaoDashboard">Página da Comissao</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/ListaMembros">
+                Lista de membros
+              </Nav.Link>
+              <Nav.Link as={Link} to="/ComissaoDashboard">
+                Página da Comissao
+              </Nav.Link>
             </Nav>
-            <Navbar.Collapse className="justify-content-end">
-                <Navbar.Brand href="#home">
-                    User.222
-                    <img
-                        alt=""
-                        src="/img/logo.svg"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />{' '}
-                </Navbar.Brand>
+            <Navbar.Collapse>
+              <Navbar.Brand href="#home">
+                User.222
+                <img
+                  alt=""
+                  src="/img/logo.svg"
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-center"
+                />{" "}
+              </Navbar.Brand>
             </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        </>
-    );
+          </div>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default NavBarADM;
